b325f2625bfbd054a620bc45f5afcbd4313ccdab
Martin Duhem <martin.duhem@gmail.com>
2017-07-05 13:49:42 +0200

    Documentation fixes in `LibraryManagementInterface`

--- core/src/main/scala/sbt/librarymanagement/LibraryManagementInterface.scala
+++ core/src/main/scala/sbt/librarymanagement/LibraryManagementInterface.scala
@@ -12,6 +12,7 @@ abstract class LibraryManagementInterface {
    * Builds a ModuleDescriptor that describes a subproject with dependencies.
    *
    * @param moduleSetting It contains the information about the module including the dependencies.
+   * @return A `ModuleDescriptor` describing a subproject and its dependencies.
    */
   def buildModule(moduleSetting: InlineConfiguration): ModuleDescriptor
 
@@ -33,34 +34,47 @@ abstract class LibraryManagementInterface {
   /**
    * Publishes the given module.
    *
-   * @param module The module to be resolved.
+   * @param module The module to be published.
    * @param configuration The publish configuration.
    * @param log The logger.
    */
   def publish(module: ModuleDescriptor, configuration: PublishConfiguration, log: Logger): Unit
 
   /**
-   * Makes the .ivy file for the given module.
+   * Makes the `ivy.xml` file for the given module.
    *
-   * @param module The module to be resolved.
+   * @param module The module for which a `ivy.xml` file is to be created.
    * @param configuration The makeIvyFile configuration.
    * @param log The logger.
+   * @return The `File` containing the Ivy description.
    */
   def makeIvyFile(module: ModuleDescriptor, configuration: DeliverConfiguration, log: Logger): File
 
   /**
-   * Makes the .pom file for the given module.
+   * Makes the `pom.xml` file for the given module.
    *
-   * @param module The module to be resolved.
-   * @param configuration The makeIvyFile configuration.
+   * @param module The module for which a `.pom` file is to be created.
+   * @param configuration The makePomFile configuration.
    * @param log The logger.
+   * @return The `File` containing the POM descriptor.
    */
   def makePomFile(module: ModuleDescriptor, configuration: MakePomConfiguration, log: Logger): File
 }
 
-object LibraryManagementInterface
-
+/**
+ * Decribes the representation of a module, inclding its dependencies
+ * and the version of Scala it uses, if any.
+ */
 trait ModuleDescriptor {
+
+  /**
+   * The direct dependencies of this module.
+   */
   def directDependencies: Vector[ModuleID]
+
+  /**
+   * The information and module about the scala version that this module requires,
+   * if any.
+   */
   def scalaModuleInfo: Option[ScalaModuleInfo]
 }
