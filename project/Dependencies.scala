cbc8fb03a14ed85c966c50f509c8971849ef9668
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2018-02-03 19:57:51 +0100

    Use newest coursier release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
   private val ioVersion = "1.1.3"
   private val utilVersion = "1.1.2"
-  private val coursierVersion = "1.0.1-M1"
+  private val coursierVersion = "1.0.1"
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
