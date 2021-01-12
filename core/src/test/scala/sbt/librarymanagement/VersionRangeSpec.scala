7eedfc037f3ebad3b5947de1cc012185def75d2f
Michael Berger <michael.goetz.berger@telekom.de>
2021-01-12 14:31:05 +0100

    Fix bug in version range handling
    https://github.com/sbt/sbt/issues/6244

--- core/src/test/scala/sbt/librarymanagement/VersionRangeSpec.scala
+++ core/src/test/scala/sbt/librarymanagement/VersionRangeSpec.scala
@@ -10,7 +10,7 @@ class VersionRangeSpec extends UnitSpec {
   it should "strip [1.0,) to 1.0" in stripTo("[1.0,)", Some("1.0"))
   it should "strip (1.0,) to 1.0" in stripTo("(1.0,)", Some("1.0"))
   it should "strip (1.0,2.0) to None" in stripTo("(1.0,2.0)", None)
-  it should "strip [1.0,2.0] to None" in stripTo("[1.0,2.0]", None)
+  it should "strip [1.0,2.0] to 2.0" in stripTo("[1.0,2.0]", Some("2.0"))
   it should "strip (,1.0],[1.2,) to 1.0" in stripTo("(,1.0],[1.2,)", Some("1.0"))
   it should "strip (,1.1),(1.1,) to None" in stripTo("(,1.1),(1.1,)", None)
 
