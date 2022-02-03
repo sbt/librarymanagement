7f6cf0e939a973873dfc792593395e84cc3021e0
Marconi Lanna <marconilanna@users.noreply.github.com>
2022-02-03 11:14:04 -0500

    Add commonly used values to License

--- core/src/main/scala/sbt/librarymanagement/License.scala
+++ core/src/main/scala/sbt/librarymanagement/License.scala
@@ -14,11 +14,23 @@ object License {
   lazy val MIT: (String, URL) =
     ("MIT", new URL("https://opensource.org/licenses/MIT"))
 
+  lazy val BSD_2_Clause: (String, URL) =
+    ("BSD-2-Clause", new URL("https://opensource.org/licenses/BSD-2-Clause"))
+
+  lazy val BSD_3_Clause: (String, URL) =
+    ("BSD-3-Clause", new URL("https://opensource.org/licenses/BSD-3-Clause"))
+
   lazy val CC0: (String, URL) =
     ("CC0-1.0", new URL("https://creativecommons.org/publicdomain/zero/1.0/legalcode"))
 
   def PublicDomain: (String, URL) = CC0
 
+  lazy val GPL2_or_later: (String, URL) =
+    ("GPL-2.0-or-later", new URL("https://www.gnu.org/licenses/gpl-2.0.html"))
+
   lazy val GPL3_or_later: (String, URL) =
     ("GPL-3.0-or-later", new URL("https://spdx.org/licenses/GPL-3.0-or-later.html"))
+
+  lazy val LGPL2_1_or_later: (String, URL) =
+    ("LGPL-2.1-or-later", new URL("https://www.gnu.org/licenses/lgpl-2.1.html"))
 }
