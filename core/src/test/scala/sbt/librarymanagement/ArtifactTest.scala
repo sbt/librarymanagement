5d2468c2aa6f5a0912e025acf30535e5bf0fe590
João Ferreira <jtjeferreira@gmail.com>
2024-05-22 11:20:37 +0100

    problem: altought mima does not complain, Artifact is not backwards compatible because Option[URI] and Option[URL] erase to Option...

--- core/src/test/scala/sbt/librarymanagement/ArtifactTest.scala
+++ core/src/test/scala/sbt/librarymanagement/ArtifactTest.scala
@@ -2,13 +2,13 @@ package sbt.librarymanagement
 
 import sjsonnew.support.scalajson.unsafe.{ CompactPrinter, Converter, Parser }
 
-import java.net.URI
+import java.net.{ URI, URL }
 
 object ArtifactTest extends verify.BasicTestSuite {
 
   test("it should format itself into JSON") {
     import LibraryManagementCodec._
-    val json = Converter.toJson(Artifact("scala-library").withUri(Some(new URI("")))).get
+    val json = Converter.toJson(Artifact("scala-library").withUri(Some(new URI("http:/")))).get
     assert(CompactPrinter(json) == expectedJson)
   }
 
@@ -16,9 +16,36 @@ object ArtifactTest extends verify.BasicTestSuite {
     import LibraryManagementCodec._
     val json = Parser.parseUnsafe(expectedJson)
     val m = Converter.fromJsonUnsafe[Artifact](json)
-    assert(m == Artifact("scala-library").withUri(Some(new URI(""))))
+    assert(m == Artifact("scala-library").withUri(Some(new URI("http:/"))))
+  }
+
+  test("it should be backwards compatible") {
+    assert(
+      Artifact(
+        "scala-library",
+        "jar",
+        "jar",
+        None,
+        Vector.empty,
+        Some(new URI("http:/")),
+        Map.empty,
+        None,
+        false
+      ) ==
+        Artifact(
+          "scala-library",
+          "jar",
+          "jar",
+          None,
+          Vector.empty,
+          Some(new URL("http:/")),
+          Map.empty,
+          None,
+          false
+        )
+    )
   }
 
   def expectedJson =
-    """{"name":"scala-library","type":"jar","extension":"jar","configurations":[],"url":"","extraAttributes":{},"allowInsecureProtocol":false}"""
+    """{"name":"scala-library","type":"jar","extension":"jar","configurations":[],"url":"http:/","extraAttributes":{},"allowInsecureProtocol":false}"""
 }
