0eb0a9a26213d746edfa37dc47b2c4013b413cad
Scala Steward <scala_steward@virtuslab.com>
2025-10-07 19:02:19 +0000

    Update scala-compiler, scala-library, ... to 2.13.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.contraband.ContrabandPlugin.autoImport._
 
 object Dependencies {
   val scala212 = "2.12.20"
-  val scala213 = "2.13.16"
+  val scala213 = "2.13.17"
 
   def nightlyVersion: Option[String] =
     sys.env.get("BUILD_VERSION") orElse sys.props.get("sbt.build.version")
