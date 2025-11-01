ae8324c1325dbfcd45e21b39f062f31085094fa3
Eugene Yokota <eed3si9n@gmail.com>
2025-11-01 16:33:46 -0400

    [1.x] Add Scala 3.8 REPL deps
    This defines Scala 3.8 REPL dependency.

--- core/src/main/scala/sbt/librarymanagement/ConfigurationExtra.scala
+++ core/src/main/scala/sbt/librarymanagement/ConfigurationExtra.scala
@@ -52,6 +52,7 @@ object Configurations {
 
   lazy val ScalaTool = Configuration.of("ScalaTool", "scala-tool").hide
   lazy val ScalaDocTool = Configuration.of("ScalaDocTool", "scala-doc-tool").hide
+  lazy val ScalaReplTool = Configuration.of("ScalaReplTool", "scala-repl-tool").hide
   lazy val CompilerPlugin = Configuration.of("CompilerPlugin", "plugin").hide
   lazy val Component = Configuration.of("Component", "component").hide
 
