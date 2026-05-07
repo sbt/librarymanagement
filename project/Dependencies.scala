d9df2196724654b770442d189d1df1f49021a13e
Scala Steward <scala_steward@virtuslab.com>
2026-05-07 17:10:38 +0000

    Update io to 1.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   def nightlyVersion: Option[String] =
     sys.env.get("BUILD_VERSION") orElse sys.props.get("sbt.build.version")
 
-  private val ioVersion = nightlyVersion.getOrElse("1.10.5")
+  private val ioVersion = nightlyVersion.getOrElse("1.12.0")
   private val utilVersion = nightlyVersion.getOrElse("1.12.11")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
