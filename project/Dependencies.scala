c1a9409ac648dcf7aed4a5c8a1859da0934e0dd8
Scala Steward <scala_steward@virtuslab.com>
2026-09-05 00:37:13 +0000

    Update scala-library to 3.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.contraband.ContrabandPlugin.autoImport._
 
 object Dependencies {
   val scala212 = "2.12.21"
-  val scala213 = "2.13.18"
+  val scala213 = "3.9.0"
 
   def nightlyVersion: Option[String] =
     sys.env.get("BUILD_VERSION") orElse sys.props.get("sbt.build.version")
